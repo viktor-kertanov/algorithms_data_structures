645c6f8f7e5caf507c7b22376a82edbbb5c7e660
Viktor Kertanov <thelonius19@gmail.com>
2023-02-13 19:38:19 +0300

    Linked Lists. Pop, pop first, prepend, append methods

--- ch_4_linked_lists/1_linked_lists.py
+++ ch_4_linked_lists/1_linked_lists.py
@@ -27,19 +27,75 @@ class LinkedList:
         
         return True
         
-    
-    def prepend(self, value):
-        pass
-   
 
-    def insert(self, index, value):
-        pass  
+    def pop(self):
+        if not self.head:   
+            return None
+        
+        elif self.length == 1:
+            node_to_return = self.head
+
+            self.head = None  
+            self.tail = None
+            self.length = 0
+            
+            return node_to_return
+        
+        else:
+            current = self.head
+            while current.next:
+                previous = current
+                current = current.next
+            
+            node_to_return = previous.next
+            
+            self.tail = previous  
+            self.tail.next = None
+            self.length -= 1
+        
+        return node_to_return
     
+
+    def prepend(self, value):
+        new_node = Node(value)
+
+        if not self.head:
+            self.head = new_node
+            self.tail = new_node
+            self.length = 1
+        
+        else:
+            new_node.next = self.head
+            self.head = new_node
+            self.length += 1
+
+        return True
+    
+
+    def pop_first(self):
+        if not self.head.next and not self.tail.next:
+            return None
+        
+        if self.length == 1:
+            node_to_return = self.head
+            self.head = None
+            self.tail = None
+            self.length = 1
+            
+            return node_to_return
+        
+        node_to_return = self.head
+        self.head = node_to_return.next
+        self.length -= 1
+
+        return node_to_return
+
     def make_empty(self):
         self.head = None
         self.tail = None
         self.length = 0
      
+    
     def print_list(self):
         temp = self.head
         while temp:
@@ -49,8 +105,35 @@ class LinkedList:
 
 if __name__ == '__main__':
     my_ll = LinkedList(1)
-    my_ll.append(2)
-    my_ll.append(3)
-    my_ll.append(3)
+    for i in range(7):
+        my_ll.append(i)
+    my_ll.append(888)
+    my_ll.append(999)
     my_ll.print_list()
+    print(f"\nLinked list has {my_ll.length} elements")
+    
+    last_el = my_ll.pop()
+    print(f"\nThe last value in the linked list before pop operation was: {last_el.value}")
+    print('--'*30)
+    print(f"\nThe new list has {my_ll.length} elements")
+    
+    my_ll.print_list()
+
+    for _ in range(my_ll.length):
+        print(f"Deleting the last value {my_ll.pop().value}")
+    
+    my_ll.print_list()
+
+    my_ll.prepend(321)
+    my_ll.prepend(456)
+    my_ll.prepend(789)
+    my_ll.prepend(1024)
+    my_ll.print_list()
+
+    print(f"Pop item: {my_ll.pop_first().value}")
+    print(f"Pop item: {my_ll.pop_first().value}")
+    print(f"Pop item: {my_ll.pop_first().value}")
+
+    my_ll.print_list()
+
     print('Hello world!')
